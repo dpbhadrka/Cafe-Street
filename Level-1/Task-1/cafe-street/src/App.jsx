a21df371ecf27f61910ed0d2513201a9279c7f7e
Divyesh Bhadrka <p.bhadrka7@gecg28.ac.in>
2023-07-19 20:34:20 +0530

    Footer component completed.

--- Level-1/Task-1/cafe-street/src/App.jsx
+++ Level-1/Task-1/cafe-street/src/App.jsx
@@ -5,6 +5,7 @@ import Popular from "./component/Popular/Popular";
 import Delivery from "./component/Delivery/Delivery";
 import AboutUs from "./component/AboutUs/AboutUs";
 import CoffeeMenu from "./component/CoffeeMenu/CoffeeMenu";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Delivery />
         <CoffeeMenu />
         <AboutUs />
+        <Footer />
       </div>
     </>
   );
---
+++ Level-1/Task-1/cafe-street/src/component/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./footer.css";
+
+export default function Divyesh() {
+  return (
+    <>
+      <footer>
+        <div className="footer">
+          <p>
+            <i>cafe</i> street
+          </p>
+          <p>123 Main Street,GEC Boys Hostel, Sector 28, Gandhinagar-382028.</p>
+          <p>
+            Thanks for visiting <i> cafe</i> street.
+          </p>
+          <p>Have a Great Day Ahead.</p>
+        </div>
+      </footer>
+    </>
+  );
+}
