90a10db45ed5810c563a2cb8252c3f6b82c5602a
Divyesh Bhadrka <p.bhadrka7@gecg28.ac.in>
2023-09-08 21:05:54 +0530

    Upadated: Path of coffee images

--- assets/Data/Menu.jsx
+++ assets/Data/Menu.jsx
@@ -2,63 +2,63 @@ const Menu = [
   {
     name: "Indian Tea",
     price: 7,
-    path: "/src/images/Indian-Tea.png",
+    path: "./images/Indian-Tea.png",
   },
   {
     name: "Cappuccino",
     price: 10,
-    path: "/src/images/Cappuccino.jpg",
+    path: "./images/Cappuccino.jpg",
   },
   {
     name: "Espresso",
     price: 5,
-    path: "/src/images/Espresso.png",
+    path: "./images/Espresso.png",
   },
   {
     name: "Hazelnut Latte",
     price: 6,
-    path: "/src/images/Hazelnut-latte.png",
+    path: "./images/Hazelnut-latte.png",
   },
   {
     name: "Vanilla Latte",
     price: 5.5,
-    path: "/src/images/Vanilla-latte.png",
+    path: "./images/Vanilla-latte.png",
   },
 
   {
     name: "Borgia",
     price: 5,
-    path: "/src/images/Borgia.png",
+    path: "./images/Borgia.png",
   },
   {
     name: "Americano",
     price: 3,
-    path: "/src/images/Americano.png",
+    path: "./images/Americano.png",
   },
   {
     name: "Frappe",
     price: 6,
-    path: "/src/images/Frappe.png",
+    path: "./images/Frappe.png",
   },
   {
     name: "Latte",
     price: 5,
-    path: "/src/images/Latte.png",
+    path: "./images/Latte.png",
   },
   {
     name: "Long-Black",
     price: 10,
-    path: "/src/images/Long-Black.png",
+    path: "./images/Long-Black.png",
   },
   {
     name: "Mocha",
     price: 6,
-    path: "/src/images/Mocha.png",
+    path: "./images/Mocha.png",
   },
   {
     name: "Vienna-Mocha",
     price: 7,
-    path: "/src/images/Vienna-Mocha.png",
+    path: "./images/Vienna-Mocha.png",
   },
 ];
 
